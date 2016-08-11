639aa638d7a4cd42be38b87d2c04dbceb057715d
gustavoelizalde <gustavoelizalde@gmail.com>
2016-08-10 21:49:59 -0400

    Active Admin installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :usercompanies
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
