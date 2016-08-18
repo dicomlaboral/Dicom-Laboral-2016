0b27f159a8212a82ab35fd252332c569bfdd564b
gustavoelizalde <gustavoelizalde@gmail.com>
2016-08-17 21:07:13 -0300

    User Active admin view table improved

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -12,6 +12,14 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
+	index do
+		selectable_column
+	    id_column
+	    column :firstname
+	    column :email
+	    columns :rut
+	    column :created_at
+	    actions
+	end
 
 end
