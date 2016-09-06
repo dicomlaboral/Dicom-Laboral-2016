a7e8d984f48e2970a4c933a51b9725fbe45a43be
Oscar Contreras <ocmchile@gmail.com>
2016-09-05 21:32:55 -0300

    search user in companies with link to new worker

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def search
   	# @user = User.where("dni = ?", params[:rut])
-  	@user = User.find_by("dni = ?", params[:rut])
+  	# @user = User.find_by("dni = ?", params[:rut])
   	# @user = User.joins(:works).where("dni = ? and company_id = 2", params[:rut]).first
   	# @user = User.joins("LEFT JOIN works ON users.id = works.user_id").select("users.*, works.*")
   	# @user = User.joins("LEFT JOIN works ON users.id = works.user_id").select("users.*, count(works) as work_count").group(:works)
+  	@user = User.joins("LEFT JOIN works ON users.id = works.user_id AND dni = '" + params[:rut] + "' AND company_id = 2").select("users.*, works.*").first
   end
 end
