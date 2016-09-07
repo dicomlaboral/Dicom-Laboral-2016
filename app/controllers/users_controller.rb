899da3058ef34f248d88adc6ba9797522e6249d1
Oscar Contreras <ocmchile@gmail.com>
2016-09-06 21:49:53 -0300

    fix search user in companies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
   	# @user = User.joins(:works).where("dni = ? and company_id = 2", params[:rut]).first
   	# @user = User.joins("LEFT JOIN works ON users.id = works.user_id").select("users.*, works.*")
   	# @user = User.joins("LEFT JOIN works ON users.id = works.user_id").select("users.*, count(works) as work_count").group(:works)
-  	@user = User.joins("LEFT JOIN works ON users.id = works.user_id AND dni = '" + params[:rut] + "' AND company_id = 2").select("users.*, works.*").first
+  	@user = User.joins("LEFT JOIN works ON users.id = works.user_id and works.company_id = 9").where("users.dni = '" + params[:rut] + "'").select("users.*, works.*").first
   end
 end
