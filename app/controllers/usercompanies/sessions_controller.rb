472e7ad97ac896328e31bd4c17599f3b29e26c31
gustavoelizalde <gustavoelizalde@gmail.com>
2016-09-13 19:15:19 -0300

    Añadir Logout a Company Dashboard

--- app/controllers/usercompanies/sessions_controller.rb
+++ app/controllers/usercompanies/sessions_controller.rb
@@ -6,7 +6,7 @@ class Usercompanies::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for(resource)
-    home_usercompanies_path
+    root_path
   end
 
   # GET /resource/sign_in
