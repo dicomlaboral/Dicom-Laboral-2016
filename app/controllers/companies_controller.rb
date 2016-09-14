f3c39d80003bb8cfd3b6bdd930588c88e0bb0b42
Oscar Contreras <ocmchile@gmail.com>
2016-09-13 21:36:47 -0300

    add dashboard to user

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -31,4 +31,17 @@ class CompaniesController < ApplicationController
     render json: @work.as_json(include: :company)
   end
 
+  def search
+    # @user = User.where("dni = ?", params[:rut])
+    # @user = User.find_by("dni = ?", params[:rut])
+    # @user = User.joins(:works).where("dni = ? and company_id = 2", params[:rut]).first
+    # @user = User.joins("LEFT JOIN works ON users.id = works.user_id").select("users.*, works.*")
+    # @user = User.joins("LEFT JOIN works ON users.id = works.user_id").select("users.*, count(works) as work_count").group(:works)
+    # @user = User.joins("LEFT JOIN works ON users.id = works.user_id and works.company_id = 9").where("users.dni = '" + params[:rut] + "'").select("users.*, works.*").first
+    @user = User.joins("LEFT JOIN works ON users.id = works.user_id and works.company_id = #{current_usercompany.company_id}").where("users.dni = '" + params[:rut] + "'").first
+    if @user.present?
+      @work = @user.works
+    end
+  end
+
 end
