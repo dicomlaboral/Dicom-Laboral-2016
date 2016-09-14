bf6490c01133b3c48a9882e5588c6e8a8ad45bcd
gustavoelizalde <gustavoelizalde@gmail.com>
2016-09-13 21:08:45 -0300

    añadiendo logo a company

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913222753) do
+ActiveRecord::Schema.define(version: 20160913233103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160913222753) do
     t.datetime "updated_at",                  null: false
     t.boolean  "active",      default: false
     t.text     "description"
+    t.string   "logo"
   end
 
   create_table "templates", force: :cascade do |t|
