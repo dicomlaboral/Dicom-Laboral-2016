e91e07c3d6faf2ed2d473debb95b9f31eaea6022
gustavoelizalde <gustavoelizalde@gmail.com>
2016-09-20 23:00:54 -0300

    add Bullet gem to detect n+1 query

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem 'rails_12factor'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'uniform_notifier'
+  gem 'bullet'
 end
 
 group :development do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,21 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.growl = false
+    Bullet.xmpp = false
+    Bullet.rails_logger = true
+    Bullet.honeybadger = false
+    Bullet.bugsnag = false
+    Bullet.airbrake = false
+    Bullet.rollbar = false
+    Bullet.add_footer = true
+  end
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = false #se dejo en false para que no envie correo el hacer db:seed
