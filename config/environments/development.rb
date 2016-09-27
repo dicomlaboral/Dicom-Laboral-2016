7614140be3a90199fdfa917ea70439f21d625803
Oscar Contreras <oscarinformatico@gmail.com>
2016-09-27 18:52:28 -0300

    sendgrid ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false #se dejo en false para que no envie correo el hacer db:seed
+  config.action_mailer.perform_deliveries = true #se dejo en false para que no envie correo el hacer db:seed
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
